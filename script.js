1132676025074a84be64df2489129d6932e0292c
YusufIKhwan <91899067+YusufIKhwan@users.noreply.github.com>
2021-11-23 11:35:48 +0800

    js 04

---
+++ script.js
@@ -0,0 +1,26 @@
+function func() {
+    alert("Page is Safe");
+}
+
+function fokus(f) {
+    x.style.background = "blue";
+}
+
+function blurx(b) {
+    y.style.background = "ligth green";
+}
+
+function funct() {
+    alert("Saya Alert Box!");
+}
+
+function fun() {
+    confirm("Saya adalah Comfirm box");
+}
+function prom() {
+    var person = prompt("Sila Isi Nama Penuh");
+    if (person != null) {
+        document.getElementById("demo").innerHTML =
+            "Hello " + person + "! How are you today?";
+    }
+}
